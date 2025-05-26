99815397337c95e38ac1fdca8ffb74253435a4c2
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-26 10:10:33 +0200

    refactor

--- src/main/scala/Yacht.scala
+++ src/main/scala/Yacht.scala
@@ -1,12 +1,12 @@
 object Yacht:
    def score(dice: List[Int], category: String): Int =
-      val ds = List("ones", "twos", "threes", "fours", "fives", "sixes")
+      val (dv, ds) = (dice.groupBy(identity).values, List("ones", "twos", "threes", "fours", "fives", "sixes"))
       category match
-         case "yacht"                 => if dice.distinct.size == 1 then 50 else 0
-         case cat if ds.contains(cat) => dice.filter(_ == ds.indexOf(cat) + 1).sum
-         case "full house"            => if dice.groupBy(identity).values.map(_.size).toSeq.sorted == Seq(2, 3) then dice.sum else 0
-         case "four of a kind"        => dice.groupBy(identity).collect { case (_, xs) if xs.size >= 4 => xs.take(4).sum }.maxOption.getOrElse(0)
-         case "little straight"       => if dice.sorted == List(1, 2, 3, 4, 5) then 30 else 0
-         case "big straight"          => if dice.sorted == List(2, 3, 4, 5, 6) then 30 else 0
-         case "choice"                => dice.sum
-         case _                       => 0
+         case "yacht"             => if dice.distinct.size == 1 then 50 else 0
+         case c if ds.contains(c) => dice.filter(_ == ds.indexOf(c) + 1).sum
+         case "full house"        => if dv.map(_.size).toSet == Set(2, 3) then dice.sum else 0
+         case "four of a kind"    => dv.filter(_.size >= 4).flatten.take(4).sum
+         case "little straight"   => if dice.sorted == (1 to 5) then 30 else 0
+         case "big straight"      => if dice.sorted == (2 to 6) then 30 else 0
+         case "choice"            => dice.sum
+         case _                   => 0
