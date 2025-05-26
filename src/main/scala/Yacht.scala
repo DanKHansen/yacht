41e4f4b0268619850c0488c528432b6dce1b4caf
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-26 09:36:55 +0200

    refactor

--- src/main/scala/Yacht.scala
+++ src/main/scala/Yacht.scala
@@ -1,19 +1,14 @@
 object Yacht:
-
    def score(dice: List[Int], category: String): Int =
-      val dg = dice.groupBy(identity)
+      val ds = List("ones", "twos", "threes", "fours", "fives", "sixes")
       category match
-         case "yacht"           => if dice.forall(_ == dice.head) then 50 else 0
-         case "ones"            => dice.filter(_ == 1).sum
-         case "twos"            => dice.filter(_ == 2).sum
-         case "threes"          => dice.filter(_ == 3).sum
-         case "fours"           => dice.filter(_ == 4).sum
-         case "fives"           => dice.filter(_ == 5).sum
-         case "sixes"           => dice.filter(_ == 6).sum
-         case "full house"      => if dg.values.map(_.size).toSeq.sorted == Seq(2, 3) then dice.sum else 0
-         case "four of a kind"  =>
-            if dg.exists(_._2.size >= 4) then dg.filter(_._2.size >= 4).map(_._2.take(4).sum).max else 0
-         case "little straight" => if dice.sorted == List(1, 2, 3, 4, 5) then 30 else 0
-         case "big straight"    => if dice.sorted == List(2, 3, 4, 5, 6) then 30 else 0
-         case "choice"          => dice.sum
-         case _                 => 0
+         case "yacht"                 => if dice.forall(_ == dice.head) then 50 else 0
+         case cat if ds.contains(cat) => dice.filter(_ == ds.indexOf(cat) + 1).sum
+         case "full house"            =>
+            if dice.groupBy(identity).values.map(_.size).toSeq.sorted == Seq(2, 3) then dice.sum else 0
+         case "four of a kind"        =>
+            dice.groupBy(identity).collect { case (_, xs) if xs.size >= 4 => xs.take(4).sum }.maxOption.getOrElse(0)
+         case "little straight"       => if dice.sorted == List(1, 2, 3, 4, 5) then 30 else 0
+         case "big straight"          => if dice.sorted == List(2, 3, 4, 5, 6) then 30 else 0
+         case "choice"                => dice.sum
+         case _                       => 0
